d0ebb5e6d2160262917cb07543b0cd59ef31ea69
Fred Tieken <fred.tieken@elision.nl>
2026-01-13 14:38:22 +0100

    Initial version

---
+++ src/main/java/com/manning/sbip/ch03/controller/SimpleController.java
@@ -0,0 +1,20 @@
+package com.manning.sbip.ch03.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SimpleController {
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "home";
+    }
+}
