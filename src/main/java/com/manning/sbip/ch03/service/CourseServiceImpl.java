49f2146cd8b36bc65d6a7b4ad46c901a8ef31527
Fred Tieken <fred.tieken@elision.nl>
2026-01-15 14:01:02 +0100

    Added webservice

---
+++ src/main/java/com/manning/sbip/ch03/service/CourseServiceImpl.java
@@ -0,0 +1,62 @@
+package com.manning.sbip.ch03.service;
+
+import com.manning.sbip.ch03.exception.CourseNotFoundException;
+import com.manning.sbip.ch03.model.Course;
+import com.manning.sbip.ch03.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+//imports
+
+@Service
+public class CourseServiceImpl implements CourseService {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+
+    @Override
+    public Course createCourse(Course course) {
+        return courseRepository.save(course);
+    }
+
+    @Override
+    public Optional<Course> getCourseById(long courseId) {
+        return courseRepository.findById(courseId);
+    }
+
+    @Override
+    public Iterable<Course> getCoursesByCategory(String category) {
+        return courseRepository.findAllByCategory(category);
+    }
+
+    @Override
+    public Iterable<Course> getCourses() {
+        return courseRepository.findAll();
+    }
+
+    @Override
+    public Course updateCourse(long courseId, Course course) {
+
+        Course existingCourse = courseRepository.findById(courseId).orElseThrow(() ->
+                new CourseNotFoundException(String.format("not found %s", courseId)));
+        existingCourse.setName(course.getName());
+        existingCourse.setCategory(course.getCategory());
+        existingCourse.setDescription(course.getDescription());
+        existingCourse.setRating(course.getRating());
+        return courseRepository.save(existingCourse);
+    }
+
+    @Override
+    public void deleteCourses() {
+        courseRepository.deleteAll();
+    }
+
+    @Override
+    public void deleteCourseById(long courseId) {
+        courseRepository.deleteById(courseId);
+    }
+
+}
